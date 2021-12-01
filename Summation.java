818e452323291b2ac0d370431bffc15402a1fb84
Sank <55155638+kattelsATWIT@users.noreply.github.com>
2021-11-30 21:14:32 -0500

    Summation

--- Summation.java
+++ Summation.java
@@ -6,8 +6,15 @@ public class Summation {
         Scanner in = new Scanner(System.in);
 
         int num = in.nextInt();
-        
-        System.out.println("Enter a integer: "+ num);
+        int num2 = in.nextInt();
+
+
+        System.out.println("Integer Entered is: "+ num);
+        System.out.println("Integer Second is: " + num2);
+
+        int num_total = (num + num2);
+
+        System.out.printf("Summation of 2 integer is: " + num_total);
 
 
     }
